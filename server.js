ddf690f1316c3d6a2fbb8e27d634b39d34786e6c
Jonathan Lee <j47lee@gmail.com>
2015-09-22 07:01:05 -0700

    files refactored

---
+++ server.js
@@ -0,0 +1,3 @@
+var weather = require('./weather.js');
+
+weather.getWeather(33.8796,117.5380);
--- weather.js
+++ weather.js
@@ -1,21 +1,24 @@
 var https = require('https');
 
-function forecast(currentSummary,currentTemp){
-  var message = 'The current summary is "' + currentSummary + '" and the temperature is ' + currentTemp + ' degrees F.';
+function forecast(timezone,currentSummary,currentTemp){
+  var message = 'The current summary for "' + timezone + '" is "' + currentSummary + '" and the temperature is ' + currentTemp + ' degrees F.';
   console.log(message);
 }
 
-var request = https.get('https://api.forecast.io/forecast/34640dc52fceb74d53adae723dfcf3f2/33.8796,-117.5380', function(res){
-  console.log(res.statusCode);
-  var info = '';
-  res.on('data', function(body){
-    info += body;
-  })
-  res.on('end', function(){
-    var profile = JSON.parse(info);
-    forecast(profile.currently.summary, profile.currently.temperature);
-  })
+function get(lat,long){
 
+  var request = https.get('https://api.forecast.io/forecast/34640dc52fceb74d53adae723dfcf3f2/' + lat + ',' + long, function(res){
+    console.log(res.statusCode);
+    var info = '';
+    res.on('data', function(body){
+      info += body;
+    })
+    res.on('end', function(){
+      var profile = JSON.parse(info);
+      forecast(profile.timezone,profile.currently.summary, profile.currently.temperature);
+    })
+  });//end https.get api request
 
+}//end get weather function
 
-});//end https.get api request
+module.exports.getWeather = get;
